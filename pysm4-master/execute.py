e320b6823cc0ed32ae9532fda09fb9dccf2e284b
Uday Indoria <uk.indoria@gmail.com>
2022-10-10 21:42:17 +0530

    checked for padding

--- pysm4-master/execute.py
+++ pysm4-master/execute.py
@@ -7,7 +7,7 @@ from pysm4 import *
 class SM4_Execution:
     plain_text = 0x0123456789abcdeffedcba9876543210
     key = 0x0123456789abcdeffedcba9876543210
-    cipher_num = 0x681edf34d206965e86b3e94f536e4246
+    #cipher_num = 0x681edf34d206965e86b3e94f536e4246
 
     cipher_text = encrypt(plain_text, key)
     dec_plain_text = decrypt(cipher_text, key)
@@ -15,7 +15,7 @@ class SM4_Execution:
     print(hex(dec_plain_text))
     print(plain_text == dec_plain_text)
 
-    print("............................................................................")
+    print("---------------------------------------------------------------------------------------------------------")
 
     plain_text = '我冷眼向过去稍稍回顾，只见它曲折灌溉的悲喜，都消失在一片亘古的荒漠，'\
                  '这才知道我的全部努力，不过完成了普通的生活。'
@@ -30,7 +30,7 @@ class SM4_Execution:
     print(dec_plain_text)
     print(plain_text == dec_plain_text)
 
-    print("............................................................................")
+    print("---------------------------------------------------------------------------------------------------------")
 
     cipher_text=encrypt_cbc(plain_text=plain_text,key=key,iv=iv)
     dec_plain_text = decrypt_cbc(cipher_text,key=key,iv=iv)
